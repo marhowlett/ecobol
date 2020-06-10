d0091ce18a03ecc9b9026e098e9491200eb79819
marhowlett <marimar_r2003@hotmail.com>
2020-06-10 12:54:08 -0500

    .

---
+++ templates/inicio.php
@@ -0,0 +1,137 @@
+<?php
+    /* Template Name: Inicio */ 
+    get_header();
+?>
+<section id="somos" class="template-section">
+       <div class="contenedor">
+       <div class="grid">
+           <div class="col"><img class="logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" alt="Logo">
+           <p>Una empresa sustentable, especializada en la fabricación y comercialización de bolsas publicitarias de tela y papel Kraft, también fabricamos cajas, para dar una solución integral al problema de los empaques, disminuyendo el impacto ambiental, sin arriesgar la calidad. 
+ </p>
+           </div>
+           <div class="col"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/somos1.png" alt="Logo"></div>
+       </div>
+    </div>
+</section>
+
+<div class="fondo">
+    <section id="productos" class="template-section">
+<div class="contenedor">
+<div class="ban">
+<h2>¡Conozca nuestros
+<span>PRODUCTOS!</span></h2>
+</div>
+<div class="ban2">
+
+<h3><img class="bolsa" src="<?php echo get_stylesheet_directory_uri(); ?>/img/bolsa.png" alt="Logo">Bolsas, Morrales, Costales, Fundas y Backpack</h3>
+</div>
+<img class="gridbolsa" src="<?php echo get_stylesheet_directory_uri(); ?>/img/Grid bolsas.png" alt="Logo">
+
+<div class="grid">
+    <div class="col">
+        <div class="ban2">
+<h3><img class="bolsa" src="<?php echo get_stylesheet_directory_uri(); ?>/img/bolsa.png" alt="Logo">Bolsas de papel kraft</h3>
+<img class="bolsa" src="<?php echo get_stylesheet_directory_uri(); ?>/img/bolsakraft.jpg" alt="Logo">
+
+</div>
+    </div>
+    <div class="col">
+        <div class="ban2">
+<h3><img class="bolsa" src="<?php echo get_stylesheet_directory_uri(); ?>/img/bolsa.png" alt="Logo">Cajas</h3>
+<img class="bolsa" src="<?php echo get_stylesheet_directory_uri(); ?>/img/caja.jpg" alt="Logo">
+</div>
+    </div>
+</div>
+<div class="center">
+<a href="/catalogo"  class="btn"> <span>VER CATÁLOGO COMPLETO</span></a>
+</div>
+</div>
+</section>
+   <section id="estampado" class="template-section"> 
+   <div class="contenedor">
+  
+   <div class="grid">
+      
+       <div class="col"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/estampado.png" alt="Logo"></div>
+        <div class="col especial"> <h2 class="verde">SERVICIO DE <span>ESTAMPADO
+PUBLICITARIO</span></h2>
+      <p>Con nuestro servicio de estampado (bordado, serigrafía o transfer) plasmamos la imagen de su negocio en nuestros productos y contribuimos al posicionamiento de su marca, ofreciéndole un contener su producto con estética, ecología y funcionalidad.</p>
+  <div class="center">
+<a href="#"  class="btn"> <span>SABER MÁS</span></a>
+</div>
+  </div>
+   </div>
+   </div>
+    </section>
+</div>
+<section id="elegir" class="template-section">
+<div class="ban">
+<h2>¿POR QUÉ
+<span>ELEGIRNOS?</span></h2>
+    </div>
+</section>
+<section id="elegir2" class="template-section">
+<div class="contenedor">
+    <div class="grid">
+        <div class="col3"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/calidad.png" alt="calidad"><p>Calidad con un menor impacto ambiental</p></div>
+        <div class="col3"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/atencion.png" alt="atencion"><p>Capacidad para atender grandes clientes</p></div>
+        <div class="col3"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/marca.png" alt="marca"><br><p>Estampado para posicionar su marca</p></div>
+        <div class="col3 ban"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/producción.png" alt="producción"><p class="verde">Si necesita algún tamaño o diseño en especial, le ayudaremos a crear una bolsa y/o caja que cumpla con sus requerimientos. </p></div>
+    </div>
+</div>
+</section>
+<div class="fondofinal">
+<section id="contacto" class="template-section">
+
+<div class="contenedor">
+   <div class="ban">
+<h2>
+<span>¡CONTÁCTANOS!</span></h2>
+    </div>
+    <div class="grid">
+        <div class="cole1"><img class="logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo2.png" alt="Logo"></div>
+        <div class="cole2"><p>En Ecobol nos esforzamos por ofrecerle bolsas y cajas de altísima calidad, para empacar su producto, sin perjudicar el medio ambiente.</p></div>
+    </div>
+     
+           <div class="grid">
+               <div class="col"><?php echo do_shortcode('[caldera_form id="CF5ecd5755c48e4"]'); ?></div>
+               
+<div class="col datos">
+       
+     
+       <div class="datos">
+       <p class="bl2"> <img class="so" src="<?php echo get_stylesheet_directory_uri(); ?>/img/clock.png" alt="Logo">  L - V 8:00 a 18:00 hrs</p>
+        
+        </div>
+        <div class="datos">
+       <p class="bl2"> <img class="so" src="<?php echo get_stylesheet_directory_uri(); ?>/img/phone.png" alt="Logo">  <a href="https://wa.me/523316030969">33 160 309 69</a></p>
+        
+        </div>
+        <div class="datos">
+       <p class="bl2"> <img class="so" src="<?php echo get_stylesheet_directory_uri(); ?>/img/WA.png" alt="Logo">  <a href="https://wa.me/523316030969">33 160 309 69</a></p>
+        
+        </div>
+        <div class="datos">
+       <p class="bl2"> <img class="so" src="<?php echo get_stylesheet_directory_uri(); ?>/img/location.png" alt="Logo">San Marcos, Guerrero
+C.P. 39960</p>
+        
+        </div>
+        <div class="datos">
+       <p class="bl2"> <img class="so" src="<?php echo get_stylesheet_directory_uri(); ?>/img/mail.png" alt="Logo">  <a href="mailto:contacto@sp2000.com.mx">contacto@sp2000.com.mx</a></p>
+        
+        </div>
+        
+     
+       <p class="negritas">SÍGUENOS EN:</p>
+       <p><a href=""><img class="so" src="<?php echo get_stylesheet_directory_uri(); ?>/img/fb.png" alt="Logo">  </a><a href="#"><img class="so" src="<?php echo get_stylesheet_directory_uri(); ?>/img/ig.png" alt="Logo">  </a><a href="#"><img class="so" src="<?php echo get_stylesheet_directory_uri(); ?>/img/yt.png" alt="Logo">  </a></p>
+       
+     </div>
+           </div>
+</div>
+</section>
+<?php include("esbrillante2.php"); ?>
+</div>
+
+<?php 
+    get_footer();
+?>
